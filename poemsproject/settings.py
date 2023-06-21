bd23c6e20ed87ca287df92eb373e2f0c29942cd5
vidito <niamadpour@gmail.com>
2023-06-21 23:17:24 +0200

    s

--- poemsproject/settings.py
+++ poemsproject/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
